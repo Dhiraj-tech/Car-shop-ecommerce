e0d0c58b3689c6e668250564e8a3c831f2f86807
Dhiraj-tech <dhirajrayofficial@gmail.com>
2023-08-07 01:02:52 +0545

    Product card added in home page

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,31 +1,35 @@
-import Image from 'next/image'
+// import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import { useEffect, useState } from 'react'
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import Badge from '@mui/material/Badge';
-import MailIcon from '@mui/icons-material/Mail';
+// import { useEffect, useState } from 'react'
+// import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import NavBar from '@/components/Navbar/index'
+import Card from '@/components/Card/index'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  const [productsList, setProductsList] = useState([])
-  const [age, setAge] = useState(34)
+  // const [productsList, setProductsList] = useState([])
+  // const [age, setAge] = useState(34)
   
-  const fetchAllProducts = async( ) => {
-    const res = await fetch('http://localhost:8080/products')
-    const data = await res.json()
-    setProductsList(data.productsList)
-  }
-  useEffect(() => {
-    fetchAllProducts()
-  }, [])
+  // const fetchAllProducts = async( ) => {
+  //   const res = await fetch('http://localhost:8080/products')
+  //   const data = await res.json()
+  //   setProductsList(data.productsList)
+  // }
+  // useEffect(() => {
+  //   fetchAllProducts()
+  // }, [])
 
   return (
     <div>
       <NavBar />
     <h1 className="pro-head">LATEST PRODUCTS:</h1>
-     {
+
+    <Card />
+
+
+
+     {/* {
       productsList.length> 0 ? (
         <div className='Products'>
           {productsList.map((item) => (
@@ -37,7 +41,9 @@ export default function Home() {
             </div>))}
         </div>
       ) : "loading"
-     }
+     } */}
+
+
     </div>
   )
 }
