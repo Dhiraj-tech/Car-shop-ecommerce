d93fff927b4638aca4bcb19d2d4ffa204ef95896
Dhiraj-tech <dhirajrayofficial@gmail.com>
2023-08-09 23:31:09 +0545

    Redux setup is made

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,17 +1,27 @@
-import { Inter } from 'next/font/google'
-import NavBar from '@/components/Navbar/index'
-import Card from '@/components/Card/index'
+import { Inter } from "next/font/google";
+import NavBar from "@/components/Navbar/index";
+import Card from "@/components/Card/index";
+import { useSelector } from "react-redux";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
 
+  // to check redux is working or not
+  const { name } = useSelector((state) => state.user);
+
   return (
-    <div>
-      <NavBar />
-      <h1 className="pro-head">LATEST PRODUCTS:</h1>
-      <Card />
-    </div>
-  )
+    <>
+      <div>
+        <NavBar />
+        <h1 className="pro-head">LATEST PRODUCTS:</h1>
+        <Card />
+      </div>
+
+      {/* to check redux is working or not */}
+      <div>{name}</div>;
+    </>
+  );
 }
+
 
