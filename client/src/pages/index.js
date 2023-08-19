f72a697bcc4b07694a4dd53667b1628a3f1e5a2a
Dhiraj-tech <dhirajrayofficial@gmail.com>
2023-08-19 20:11:23 +0545

    Minor changes in client

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,14 +1,10 @@
 import { Inter } from "next/font/google";
-import { useSelector } from "react-redux";
 import Home from "./home";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Main() {
-  const { isLoggedIn } = useSelector((state) => state.user);
-  if (isLoggedIn) {
-    return <Home />;
-  } else {
-    return <Home />;
-  }
+  return <Home />;
 }
+
+
