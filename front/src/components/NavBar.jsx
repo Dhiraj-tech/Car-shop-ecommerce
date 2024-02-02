1d4ca4cdec16f8f15f5b55631fcfd03e330329b4
Dhiraj-tech <dhirajrayofficial@gmail.com>
2024-02-02 09:59:05 +0545

    Final commit

--- front/src/components/NavBar.jsx
+++ front/src/components/NavBar.jsx
@@ -1,11 +1,16 @@
 import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 import http from "../http"
+import { useDispatch, useSelector } from "react-redux";
+import { clearStorage, fromStorage, isEmpty } from "../lib";
+import { clearUser, setUser } from "../store";
 
 export const NavBar = () => {
-
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 768); // Assuming tablets and phones have a maximum width of 768px
+    const user = useSelector((st) => st.user.value);
     const [categories, setCategories] = useState([])
     const [brands, setBrands] = useState([])
+    const dispatch = useDispatch();
 
     useEffect(() => {
         http.get('category')
@@ -17,6 +22,41 @@ export const NavBar = () => {
             .then(({data}) => setBrands(data))
             .catch(err => {})
     }, [])
+
+    useEffect(() => {
+        if (isEmpty(user)) {
+          const token = fromStorage("user_token");
+    
+          if (!isEmpty(token)) {
+            http
+              .get("profile/details")
+              .then(({ data }) => {
+                dispatch(setUser(data));
+              })
+              .catch((err) => {
+                clearStorage("user_token");
+              });
+          }
+        }
+      }, [user]);
+    
+      const handleLogout = (ev) => {
+        ev.preventDefault();
+    
+        dispatch(clearUser());
+        clearStorage("user_token");
+      };
+
+      useEffect(() => {
+        const handleResize = () => {
+          setIsMobile(window.innerWidth <= 768);
+        };
+    
+        window.addEventListener("resize", handleResize);
+        return () => {
+          window.removeEventListener("resize", handleResize);
+        };
+      }, []);
 
     return <div className="row">
     <nav className="navbar navbar-expand-lg navbar-light bg-white col-12">
@@ -51,6 +91,39 @@ export const NavBar = () => {
 
                     </div>
                 </li>
+                {isMobile && (
+            <>
+              {isEmpty(user) ? (
+                <>
+                  <li className="nav-item active">
+                    <Link className="nav-link" to="/register">
+                      Register
+                    </Link>
+                  </li>
+                  <li className="nav-item active">
+                    <Link className="nav-link" to="/login">
+                      Login
+                    </Link>
+                  </li>
+                </>
+              ) : (
+                <>
+                  <li className="nav-item active">
+                    <Link className="nav-link" to="/profile">
+                      <i className="fas fa-user-circle me-2"></i>
+                      {user.name}
+                    </Link>
+                  </li>
+                  <li className="nav-item active">
+                    <Link className="nav-link" onClick={handleLogout}>
+                      <i className="fas fa-sign-in-alt me-2"></i>
+                      Logout
+                    </Link>
+                  </li>
+                </>
+              )}
+            </>
+          )}
                 
             </ul>
         </div>
